b6a2bf120a4126d7679d8609be85d7d6cfb6bfe2
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-01-02 14:11:36 -0500

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCOz2xTrOh_63dgjxicGiO9fLkKH7guJvQ",
+  authDomain: "house-marketplace-app-59d89.firebaseapp.com",
+  projectId: "house-marketplace-app-59d89",
+  storageBucket: "house-marketplace-app-59d89.appspot.com",
+  messagingSenderId: "92453346123",
+  appId: "1:92453346123:web:52a973eeb9ff14e76a26ce",
+  measurementId: "G-18V8X2SKK8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore();
